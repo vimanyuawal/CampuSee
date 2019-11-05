bc1f11ce469a445865961a8eb8f931db7d3b5e5b
Vimanyu Awal <awal@usc.edu>
2019-11-05 00:59:58 -0800

    Added User Profile page

--- app/src/main/java/com/example/campuseetest/UserProfile.java
+++ app/src/main/java/com/example/campuseetest/UserProfile.java
@@ -2,25 +2,37 @@ package com.example.campuseetest;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.Editable;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.android.gms.auth.api.signin.GoogleSignIn;
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
 import com.google.android.gms.auth.api.signin.GoogleSignInClient;
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
 import com.google.firebase.FirebaseApp;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
-class UserProfile {
-
+public class UserProfile  extends AppCompatActivity {
     String identifierVal;
 
     GoogleSignInClient mGoogleSignInClient;
+    String name = "";
+    String email = "";
+
+    DatabaseReference mRootRef = FirebaseDatabase.getInstance().getReference();
+    DatabaseReference mUserRef = mRootRef.child("User");
+
+//    private FusedLocationProviderClient fusedLocationClient;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_user_home);
+        setContentView(R.layout.activity_user_profile);
 
         FirebaseApp.initializeApp(this);
 
@@ -33,11 +45,34 @@ class UserProfile {
         // Build a GoogleSignInClient with the options specified by gso.
         mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
 
-        GoogleSignInAccount acct = GoogleSignIn.getLastSignedInAccount(UserHomeActivity.this);
+        GoogleSignInAccount acct = GoogleSignIn.getLastSignedInAccount(UserProfile.this);
 
-        identifierVal = acct.getEmail();
+        name = acct.getDisplayName();
+        email = acct.getEmail();
 
-        
+        final String key = email.replace('.', ',');
+
+        EditText et = (EditText) findViewById(R.id.plain_text_input);
+        et.setText(name);
+
+
+        Button button2=findViewById(R.id.commit_profile_change);
+
+        button2.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                EditText n = (EditText) findViewById(R.id.plain_text_input);
+                String newName = n.getText().toString();
+
+                mUserRef.child(key).child("name").setValue(newName);
+
+                Intent intent= new Intent(UserProfile.this, UserHomeActivity.class);
+                startActivity(intent);
+            }
+        });
+
     }
+
+
+
 
 }
